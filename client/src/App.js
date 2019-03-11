f83cc488d009581491d2934578a6119cf70430c5
diegosoriarios <diegosoriarios@gmail.com>
2019-03-11 14:47:44 -0300

    Adding cdn

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Controlled as CodeMirror } from "react-codemirror2";
 import Pusher from "pusher-js";
 import pushid from "pushid";
 import axios from "axios";
+import { JQUERY, REACT, VUEJS } from './cdnList';
 
 import "./App.css";
 import "codemirror/lib/codemirror.css";
@@ -19,7 +20,8 @@ class App extends Component {
       id: "",
       html: "",
       css: "",
-      js: ""
+      js: "",
+      cdn: "",
     };
 
     this.pusher = new Pusher("a03a2a5d7b7919fc5462", {
@@ -60,7 +62,7 @@ class App extends Component {
   };
 
   runCode = () => {
-    const { html, css, js } = this.state;
+    const { html, css, js, cdn } = this.state;
 
     const iframe = this.refs.iframe;
     const document = iframe.contentDocument;
@@ -71,6 +73,7 @@ class App extends Component {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        ${cdn}
         <title>Document</title>
         <style>
           ${css}
@@ -90,6 +93,34 @@ class App extends Component {
     document.write(documentContents);
     document.close();
   };
+
+  handleSelect = e => {
+    console.log(e.target.value)
+    switch (e.target.value) {
+      case 'jquery':
+        console.log('Jquery')
+        this.setState({
+          cdn: `<script src="${JQUERY}"></script>`
+        })
+        break;
+      case 'react':
+        console.log('React')
+        this.setState({
+          cdn: `<script src="${REACT}"></script>`
+        })
+        break;
+      case 'vuejs':
+        console.log('Vue')
+        this.setState({
+          cdn: `<script src="${VUEJS}"></script>`
+        })
+        break;
+      default:
+        console.log('Ops')
+        break;
+    }
+    console.log(this.state.cdn)
+  }
 
   render() {
     const { html, js, css } = this.state;
@@ -130,6 +161,14 @@ class App extends Component {
             />
           </div>
           <div className="code-editor js-code">
+
+            <select onChange={this.handleSelect} value={this.state.cdn} className="select">
+              <option value=""></option>
+              <option value="jquery">Jquery</option>
+              <option value="react">React</option>
+              <option value="vuejs">VueJs</option>
+            </select>
+
             <div className="editor-header">JavaScript</div>
             <CodeMirror
               value={js}
---
+++ client/src/cdnList.js
@@ -0,0 +1,3 @@
+export const JQUERY = 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js'
+export const REACT = 'https://cdnjs.cloudflare.com/ajax/libs/react/16.8.4/umd/react.production.min.js'
+export const VUEJS = 'https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.8/vue.min.js'
